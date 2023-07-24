565c618e2d7e88169a944fc27c238fa9c684d3c9
youssoflee <63400034+youssoflee@users.noreply.github.com>
2023-07-24 17:12:33 +0800

    Lab 14 Part 3

--- lab14/src/Part3/server/controller/CustomerDataManager.java
+++ lab14/src/Part3/server/controller/CustomerDataManager.java
@@ -10,7 +10,7 @@ import Part3.model.Product;
  * Lab Week 14 Part 3 Exercise 3
  * This controller class manages customers' data.
  * 
- * @author Youssf Lee
+ * @author Youssof Lee
  */
 
 public class CustomerDataManager {
